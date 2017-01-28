e4a2159a4d450e6b3447818d227d572aed563ca5
Klaudia <klaudia@Klaudias-iMac.local>
2017-01-28 21:34:36 +0100

    add post a new todo

--- ToDoApp/AddViewController.swift
+++ ToDoApp/AddViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class AddViewController: UIViewController {
     
+    @IBOutlet weak var priorityField: UITextField!
+    @IBOutlet weak var completedFIeld: UITextField!
+    @IBOutlet weak var noteField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,9 +26,63 @@ class AddViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func wyslij() {
+        let url:String = "https://secret-badlands-37403.herokuapp.com/api/todos"
+        
+        let myUrl = URL(string: url)
+        
+        var request = URLRequest(url: myUrl!)
+        request.httpMethod = "POST"
+        
+        guard let name = nameField.text,
+            let note = noteField.text,
+            let completed = completedFIeld.text,
+            let priority = priorityField.text
+            else {
+                print("Guard is coming..")
+                return
+            }
+        
+        let mapDict = ["note" : "First", "completed" : "Second"]
+        let json = [ "name" : name, "note" : note, "completed" : completed, "priority" : priority, "dict" : mapDict] as [String : Any]
+        
+        let postString = try! JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+
+        request.httpBody = postString
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let task = URLSession.shared.dataTask(with: request) {
+            (data: Data?, response: URLResponse?, error: Error?) in
+            
+            if error != nil {
+                print("error=\(error)")
+                return
+            }
+            
+            print("response = \(response)")
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                
+                if let parseJSON = json {
+                    let message = parseJSON["message"] as? String
+                    print("message: \(message)")
+                }
+            } catch {
+                print(error)
+            }
+        }
+        
+        task.resume()
+    }
+    
     
     
     @IBAction func addNewTask(_ sender: Any) {
+        
+        wyslij()
+        
         let myAlert = UIAlertController(title: "Alert", message: "Are you sure?", preferredStyle: .actionSheet)
         
         let okAction = UIAlertAction(title: "OK", style: .default)
